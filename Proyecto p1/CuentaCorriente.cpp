e7eb78434233804a822962a2a94dda8761a2b04a
Ing. Alex <daullcu@espe.edu.ec>
2025-05-25 10:29:55 -0500

    Cuenta

---
+++ Proyecto p1/CuentaCorriente.cpp
@@ -0,0 +1,37 @@
+#include "CuentaCorriente.h"
+
+CuentaCorriente::CuentaCorriente(const char* idCuenta, const char* idUsuario, const char* nombre, double saldoInicial, double sobregiro)
+    : Cuenta(idCuenta, idUsuario, nombre, saldoInicial), limiteSobregiro(sobregiro) {
+    if (sobregiro < 0) throw "Límite de sobregiro inválido";
+}
+
+void CuentaCorriente::depositar(double monto) {
+    if (monto <= 0) throw "Monto de depósito inválido";
+    saldo += monto;
+}
+
+bool CuentaCorriente::retirar(double monto) {
+    if (monto <= 0) throw "Monto de retiro inválido";
+    if (monto > saldo + limiteSobregiro) return false;
+    saldo -= monto;
+    return true;
+}
+
+char* CuentaCorriente::obtenerTipo() const {
+    char* tipo = new char[10];
+    strcpy(tipo, "Corriente");
+    return tipo;
+}
+
+Cuenta& CuentaCorriente::operator+=(double monto) {
+    if (monto <= 0) throw "Monto de depósito inválido";
+    saldo += monto;
+    return *this;
+}
+
+bool CuentaCorriente::operator-=(double monto) {
+    if (monto <= 0) throw "Monto de retiro inválido";
+    if (monto > saldo + limiteSobregiro) return false;
+    saldo -= monto;
+    return true;
+}
